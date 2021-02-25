9cedbdaa4a701d27b7ac986a6dc95e7323ca69d1
Timon Hüppi <timon.hueppi@gmail.com>
2021-02-25 09:07:02 +0100

    adds Button Handler and Activity Call

--- app/src/main/java/ch/timonhueppi/m335/bmicalc/MainActivity.java
+++ app/src/main/java/ch/timonhueppi/m335/bmicalc/MainActivity.java
@@ -2,7 +2,9 @@ package ch.timonhueppi.m335.bmicalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setButtonHandler();
+    }
+
+    private void setButtonHandler(){
+        Button btnCalc = findViewById(R.id.btnCalc);
+        btnCalc.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ResultActivity.class);
+            startActivity(intent);
+        });
     }
 }
